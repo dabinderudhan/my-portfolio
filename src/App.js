cdf1e51f31fad89d1166e3b8df18a71e07d11a6b
dabinder.udhan@gmail.com <dabinder.udhan@gmail.com>
2022-11-26 20:15:16 +0530

    extra files deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Website under construction...</h1>
+      <header className="App-header">
+        <h1>Website under construction...</h1>
+      </header>
     </div>
   );
 }
